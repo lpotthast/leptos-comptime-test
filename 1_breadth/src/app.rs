2a50a5ada8beb2aba7ea943d1447c11a654eb4b9
Lukas Potthast <privat@lukas-potthast.de>
2023-07-30 15:44:32 +0200

    Simplify

--- 1_breadth/src/app.rs
+++ 1_breadth/src/app.rs
@@ -2,26 +2,11 @@ use leptos::*;
 
 #[component]
 pub fn App() -> impl IntoView {
-    let (show, set_show) = create_signal(false);
-    let toggle = move |_| match show.get_untracked() {
-        true => {
-            tracing::info!("hide <Test>");
-            set_show.set(false);
-        }
-        false => {
-            tracing::info!("show <Test>");
-            set_show.set(true)
-        }
-    };
-    view! {
-        <button on:click=toggle>"Toggle component"</button>
-        { move || show.get().then(|| view! { <Test/> }) }
-    }
+    view! { <Test/> }
 }
 
 #[component]
 pub fn Test() -> impl IntoView {
-    tracing::info!("initialize <Test>");
     view! {
         <A>"Sibling"</A>
         <A>"Sibling"</A>
@@ -133,7 +118,6 @@ pub fn Test() -> impl IntoView {
 
 #[component]
 pub fn A(children: Children) -> impl IntoView {
-    tracing::info!("initialize <A>");
     view! {
         <div>
             "A"
--- 4a_small_depth_with_breadth_leptos_0.3/src/app.rs
+++ 4a_small_depth_with_breadth_leptos_0.3/src/app.rs
@@ -2,26 +2,11 @@ use leptos::*;
 
 #[component]
 pub fn App(cx: Scope) -> impl IntoView {
-    let (show, set_show) = create_signal(cx, false);
-    let toggle = move |_| match show.get_untracked() {
-        true => {
-            tracing::info!("hide <Test>");
-            set_show.set(false);
-        }
-        false => {
-            tracing::info!("show <Test>");
-            set_show.set(true)
-        }
-    };
-    view! {cx,
-        <button on:click=toggle>"Toggle component"</button>
-        { move || show.get().then(|| view! {cx, <Test/> }) }
-    }
+    view! {cx, <Test/> }
 }
 
 #[component]
 pub fn Test(cx: Scope) -> impl IntoView {
-    tracing::info!("initialize <Test>");
     view! {cx,
         <A>
             <A>
@@ -56,7 +41,6 @@ pub fn Test(cx: Scope) -> impl IntoView {
 
 #[component]
 pub fn A(cx: Scope, children: Children) -> impl IntoView {
-    tracing::info!("initialize <A>");
     view! {cx,
         <div>
             "A"
